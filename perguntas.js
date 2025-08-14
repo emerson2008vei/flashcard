065d8b741d7643aa625f46bea94e00e6faecd31c
emerson2008vei <emilly.nazare.domingues@escola.pr.gov.br>
2025-08-14 15:34:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,65 @@
+criarCartao(
+    'biologia',
+ 'Qual é o maior orgão do corpo humano?' ,
+ 'A pele.'
+)
+
+criarCartao(
+    'biologia',
+    'Qual é o processo pelo qual as plantas convertem a luz solar em energia?',
+    'Fotossíntese.'
+)
+
+criarCartao(
+    'história',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca.'
+)
+
+criarCartao(
+    'química',
+    'Qual é a fórmula química da água?',
+    'H2O.'
+)
+
+criarCartao(
+    'geografia',
+    'Qual é o maior país do mundo em área territorial?',
+    'Rússia.'
+)
+
+criarCartao(
+    'matemática',
+    'Qual é o resultado de 7 x 8?',
+    '56.'
+)
+
+criarCartao(
+    'física',
+    'Qual é a lei da física que afirma que "para toda ação há uma reação igual e oposta"?',
+    'A Terceira Lei de Newton.'
+)
+
+criarCartao(
+    'biologia',
+    'Qual é o nome da estrutura celular responsável pela produção de energia?',
+    'Mitocôndria.'
+)
+
+criarCartao(
+    'história',
+    'Em que ano a Proclamação da República do Brasil aconteceu?',
+    '1889.'
+)
+
+criarCartao(
+    'química',
+    'Qual é o elemento químico mais abundante na crosta terrestre?',
+    'Oxigênio.'
+)
+
+criarCartao(
+    'geografia',
+    'Qual é o nome do deserto mais quente do mundo?',
+    'Deserto do Saara.'
+)
